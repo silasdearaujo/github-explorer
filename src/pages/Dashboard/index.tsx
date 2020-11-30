2f1513ff5f855e473feb4dba2944a9ce0fdfef2b
silas <silas.araujo@zema.com>
2020-11-30 18:55:22 -0300

    style dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -14,6 +15,21 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório"/>
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img 
+            src="https://avatars2.githubusercontent.com/u/51761976?s=460&u=59b578f2efc9e56884b8e793b432524dd8655693&v=4" 
+            alt="Silas de Araujo"
+          />
+          <div>
+            <strong>silasdearaujo</strong>
+            <p>Github explorer</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+        </a>        
+      </Repositories>
     </>
   );
 };
