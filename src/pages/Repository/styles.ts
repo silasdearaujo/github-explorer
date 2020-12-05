699b55e94d3ef65863aac8542794ff57b89d27a1
Silas de Araujo <silasdearaujo@live.com>
2020-12-05 16:15:48 -0300

    finalizando aplicacao

--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -1,72 +1,86 @@
-import styled, { css } from 'styled-components';
-import { shade } from 'polished';
+import styled from 'styled-components';
 
-interface FormProps {
-  hasError: boolean;
-}
 
-export const Title = styled.h1`
-  font-size: 48px;
-  color: #3a3a3a;
-  max-width: 450px;
-  line-height: 56px;
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-  margin-top: 80px;
-`;
-
-export const Form = styled.form<FormProps>`
-  margin-top: 40px;
-  max-width: 700px;
-
-  display: flex;
-
-  input {
-    flex: 1;
-    height: 70px;
-    padding: 0 24px;
-    border: 0;
-    border-radius: 5px 0 0 5px;
-    color: #3a3a3a;
-    border: 2px solid #fff;
-    border-right: 0;
-
-    ${props =>
-      props.hasError &&
-      css`
-        border-color: #c53030;
-      `}
-
-    &::placeholder {
+    a {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
       color: #a8a8b3;
-    }
-  }
+      transition: color 0.2s;
 
-  button {
-    width: 210px;
-    height: 70px;
-    background: #04d361;
-    border-radius: 0 5px 5px 0;
-    border: 0;
-    color: #fff;
-    font-weight: bold;
-    transition: background-color 0.2s;
+      &:hover {
+        color: #666;
+      }
 
-    &:hover {
-      background: ${shade(0.2, '#04d361')};
+      svg {
+        margin-right: 4px;
+      }
     }
-  }
 `;
 
-export const Error = styled.span`
-  display: block;
-  color: #c53030;
-  margin-top: 8px;
+export const RepositoryInfo = styled.section`
+    margin-top: 80px;
+
+    header {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;        
+      }
+
+      div {
+        margin-left: 24px;
+        
+        strong {
+          font-size: 36px;
+          color: #3d3d4d;
+        }
+
+        p {
+          font-size: 18px;
+          color: #737380;
+          margin-top: 4px;
+        }
+      }
+    }
+
+    ul {
+      display: flex;
+      list-style: none;
+      margin-top: 40px;
+
+      li {
+
+        & + li {
+          margin-left: 80px;
+        }
+
+        strong {
+          display: block;
+          font-size: 36px;
+          color: #3d3d4d;
+        }
+
+        span {
+          display: block;
+          margin-top: 4px;
+          color: #6c6c80;
+        }
+      }
+    }
 `;
 
-export const Repositories = styled.div`
+export const Issues = styled.div`
   margin-top: 80px;
-  max-width: 700px;
-
+  
   a {
     background: #fff;
     border-radius: 5px;
@@ -85,12 +99,6 @@ export const Repositories = styled.div`
 
     &:hover {
       transform: translateX(10px);
-    }
-
-    img {
-      width: 64px;
-      height: 64px;
-      border-radius: 50%;
     }
 
     div {
